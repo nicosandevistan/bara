7717db2f67c9d3fab0d03f440cbd5d953581b410
Nicolas Rojas Taborda <rojastabordanicolas@gmail.com>
2025-03-28 19:46:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -47,3 +47,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('active');
+        const answer = button.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
